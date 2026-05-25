3ee4f97a3733e7084e2831bb38206c8a92bba68b
David Reyes Ospina <dareyesos@unal.edu.co>
2026-05-25 13:38:43 -0500

    feat: se agregan las pruebas unitarias

--- src/app.py
+++ src/app.py
@@ -113,7 +113,6 @@ def delete_task(task_id):
     conn.close()
     return jsonify({"message": "Tarea eliminada"}), 200
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
